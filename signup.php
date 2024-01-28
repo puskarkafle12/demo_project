4da3916bdda5a62aeab3fcc0ae44ed553aa113fa
puskar kafle <pkafle@deerhold.com>
2024-01-28 10:15:40 +0545

    form simple username and password printing demo

--- signup.php
+++ signup.php
@@ -3,7 +3,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-
     echo "<h2>Data from POST Request:</h2>";
     echo "<p>Name: $name</p>";
     echo "<p>Email: $email</p>";
@@ -12,7 +11,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_GET['name'];
     $email = $_GET['email'];
     $password = $_GET['password'];
-
     echo "<h2>Data from GET Request:</h2>";
     echo "<p>Name: $name</p>";
     echo "<p>Email: $email</p>";
